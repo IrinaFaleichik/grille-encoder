75e67aa6a4127e52fd3c1d314c30af97335fe64b
IrinaFaleichik <ira.faleichik@gmail.com>
2025-11-29 20:00:45 +0500

    added JSON encryption/decryption to model

--- src/main/scala/domain/package.scala
+++ src/main/scala/domain/package.scala
@@ -1,13 +1,33 @@
 package irka.grilleEncoder.domain
 
 package object model {
-  final class User()
+  import zio.json.{JsonEncoder, JsonDecoder, DeriveJsonEncoder, DeriveJsonDecoder}
 
-  final class Text // to put under the cardboard?????
+  final case class User(id: Int, name: String)
 
-  final class Cardboard(lst: List[Square])
+  object User {
+    implicit val encoder: JsonEncoder[User] = DeriveJsonEncoder.gen[User]
+    implicit val decoder: JsonDecoder[User] = DeriveJsonDecoder.gen[User]
+  }
+  final class Text // todo to put under the cardboard?????
 
-  final class Square(start: Point, end: Point)
+  final case class Cardboard(lst: List[Square])
+  object Cardboard {
+    implicit val encoder: JsonEncoder[Cardboard] = DeriveJsonEncoder.gen[Cardboard]
+    implicit val decoder: JsonDecoder[Cardboard] = DeriveJsonDecoder.gen[Cardboard]
+  }
+  
+  final case class Square(start: Point, end: Point)
 
-  final class Point(x: Int, y: Int) // get from front
+    object Square {
+      implicit val encoder: JsonEncoder[Square] = DeriveJsonEncoder.gen[Square]
+      implicit val decoder: JsonDecoder[Square] = DeriveJsonDecoder.gen[Square]
+    }
+    
+  final case class Point(x: Int, y: Int) // get from front
+
+  object Point {
+    implicit val encoder: JsonEncoder[Point] = DeriveJsonEncoder.gen[Point]
+    implicit val decoder: JsonDecoder[Point] = DeriveJsonDecoder.gen[Point]
+  }
 }
