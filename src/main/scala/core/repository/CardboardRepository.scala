4303503f680576242a9a8c456f4af4541bb0eb16
IrinaFaleichik <ira.faleichik@gmail.com>
2025-12-07 18:14:08 +0500

    rewriting Repository objects

--- src/main/scala/core/repository/CardboardRepository.scala
+++ src/main/scala/core/repository/CardboardRepository.scala
@@ -3,42 +3,78 @@ package irka.grilleEncoder.core.repository
 import io.getquill.SnakeCase
 import io.getquill.jdbczio.Quill
 import irka.grilleEncoder.domain.model
-import irka.grilleEncoder.domain.model.Cardboard
-import irka.grilleEncoder.domain.model.User
-import irka.grilleEncoder.infrastructure.db.CardboardRepositoryLive
+import irka.grilleEncoder.domain.model.{Cardboard, Square, User}
+import irka.grilleEncoder.infrastructure.db.{CardboardRepositoryLive, DBService}
+import irka.grilleEncoder.infrastructure.db.entities.{CardboardRow, DBContext}
 import zio.*
 
-trait CardboardRepository {
+import scala.language.implicitConversions
+
+trait CardboardRepository(DBService: DBService) {
+
+  private def findUser(userId: String): User = ???
+  //    def countPicturesOf(topic: String): ZIO[Any, Nothing, Int]
+
   def create(cardboard: Cardboard): Task[Cardboard]
-  
-  def findById(cardboard: Cardboard): Task[Option[Cardboard]]
-  
-  def findByUserId(cardboard: Cardboard): Task[List[Cardboard]]
-  
+
+  def get(cardboard: Cardboard): Task[List[Cardboard]]
+
   def update(cardboard: Cardboard): Task[Cardboard]
-  
+
   def delete(cardboard: Cardboard): Task[Cardboard] // todo return Cardboard or number of deleted cardboards + CHECK THE DELETE IN OBJECT
-  
-  // Cascade delete when user is deleted (if not handled at DB level)
-  def deleteByUser(user: User): Task[Unit]
+
 }
+//
+//  def countPicturesOf(topic: String): ZIO[CardboardRepository, Nothing, Int] =
+//    ZIO.environmentWithZIO(_.get.countPicturesOf(topic))
+
+
+//class CardboardRepository1(ctx: Quill.Sqlite[SnakeCase]) {
+//
+//  def create(cardboard: Cardboard): Task[Cardboard]
+//
+//  def get(cardboard: Cardboard): Task[Option[Cardboard]]
+//
+//  // todo move to class by id/byUser
+//  def findById(cardboard: Cardboard): Task[Option[Cardboard]]
+//  // todo move to class by id/byUser
+//  def findByUserId(cardboard: Cardboard): Task[List[Cardboard]]
+//
+//  def update(cardboard: Cardboard): Task[Cardboard]
+//
+//  def delete(cardboard: Cardboard): Task[Cardboard] // todo return Cardboard or number of deleted cardboards + CHECK THE DELETE IN OBJECT
+//
+//  // todo move to class ByUser Cascade delete when user is deleted (if not handled at DB level)
+//  def deleteByUser(user: User): Task[Unit]
+//
+//  def live[CRImpl <: CardboardRepository1]: ZLayer[DBContext, Nothing, CRImpl] = {
+//    ZLayer.fromFunction(new CRImpl(_))
+//  }
+//}
+
+//final class CardboardRepositoryDefault {
+//
+//}
 
 // TODO implement it later, move to domain?
-object CardboardRepository {
-  def create(cardboard: Cardboard): ZIO[CardboardRepository, Throwable, Cardboard] = ???
-  
-  def findById(cardboardId: String): ZIO[CardboardRepository, Throwable, Option[Cardboard]] = ???
-  
-  def findByUserId(userId: String): ZIO[CardboardRepository, Throwable, List[Cardboard]] = ???
-  
-  def findAll: ZIO[CardboardRepository, Throwable, List[Cardboard]] = ???
-  
-  def update(cardboardId: String, name: String): ZIO[CardboardRepository, Throwable, Cardboard] = ???
-  
-  def delete(cardboardId: String): ZIO[CardboardRepository, Throwable, Unit] = ???
-  
-  def deleteByUserId(userId: String): ZIO[CardboardRepository, Throwable, Unit] = ???
-
-  ???
-  val live: ZLayer[Quill.Sqlite[SnakeCase], Nothing, CardboardRepositoryLive] = ZLayer.fromFunction(new CardboardRepositoryLive(_))
-}
+//object CardboardRepository {
+//  def create(cardboard: Cardboard): ZIO[CardboardRepository, Throwable, Cardboard] = CardboardRepositoryLive.create(cardboard)
+//
+////  def findById(cardboardId: String): ZIO[CardboardRepository, Throwable, Option[Cardboard]] = ???
+////
+////  def findByUserId(userId: String): ZIO[CardboardRepository, Throwable, List[Cardboard]] = ???
+////
+//  def get: ZIO[CardboardRepository, Throwable, List[Cardboard]] = ???
+//
+//  def update(cardboardId: String, name: String): ZIO[CardboardRepository, Throwable, Cardboard] = ???
+//
+//  def delete(cardboardId: String): ZIO[CardboardRepository, Throwable, Unit] = ???
+////
+////  def deleteByUserId(userId: String): ZIO[CardboardRepository, Throwable, Unit] = ???
+//
+//  ???
+//  lazy val live: CardboardRepository => ZLayer[Quill.Sqlite[SnakeCase], Nothing, CardboardRepository] = { from =>
+//    ZLayer.fromFunction(from)
+//  }
+//  val live: ZLayer[Quill.Sqlite[SnakeCase], Nothing, CardboardRepositoryLive] = ZLayer.fromFunction(new CardboardRepositoryLive(_))
+//}
--- src/main/scala/infrastructure/db/CardboardRepositoryLive.scala
+++ src/main/scala/infrastructure/db/CardboardRepositoryLive.scala
@@ -1,31 +1,48 @@
 package irka.grilleEncoder.infrastructure.db
 
 import irka.grilleEncoder.domain.model.{Cardboard, Square, User}
-import irka.grilleEncoder.infrastructure.db.entities.CardboardRow
+import irka.grilleEncoder.infrastructure.db.entities.{CardboardRow, DBContext}
 import io.getquill.SnakeCase
 import io.getquill.jdbczio.Quill
-import zio.ZIO
+import zio.{Task, ZIO, ZLayer}
 import io.getquill.*
+import irka.grilleEncoder.core.repository.*
 
 import java.sql.SQLException
 import zio.IsSubtypeOfError.impl
 
-class CardboardRepositoryLive(ctx: Quill.Sqlite[SnakeCase]) {
-  // Mapping logic lives here todo move to domain? find the whole user or better just return user id?
-  def findUser(userId: String): User = ???
+import scala.language.implicitConversions
 
-  private def toRow(cardboard: Cardboard): CardboardRow =
-    CardboardRow(cardboard.id, cardboard.name, cardboard.user.id)
+object CardboardRepositoryLive { //todo think please how to do or redo dummy and explicit type
 
-  private def toDomain(row: CardboardRow, squares: List[Square]): Cardboard =
-    Cardboard(row.id, row.name, squares, findUser(row.userId))
-
-  import ctx.*
-
-  def insertValues(cardboards: List[CardboardRow]) = quote {
-    liftQuery(cardboards).foreach(crd => query[CardboardRow].insertValue(crd))
+  lazy val live: ZLayer[DBService, Throwable, CardboardRepository] = ZLayer.fromFunction(makeDummy)
+  lazy val makeDummy: DBService => CardboardRepository= { dbService =>
+    CardboardRepositoryDefault(dbService)
   }
+}
 
-  def create(cardboard: List[CardboardRow]) = ctx.run(insertValues(cardboard))
+//sealed trait CardboardRepositoryLive {
+//
+//}
+
+final class CardboardRepositoryDefault(dbService: DBService) extends CardboardRepository(dbService) {
+  // Mapping logic lives here todo move to domain? find the whole user or better just return user id?
+  given toRow(using cardboard: Cardboard): CardboardRow =
+  CardboardRow(cardboard.id, cardboard.name, cardboard.user.id)
+
+  given toDomain(using row: CardboardRow, squares: List[Square]): Cardboard = Cardboard(row.id, row.name, squares, findUser(row.userId))
+
+  //todo fix implicits - move them to CardboardRepository
+//  using CardboardRepository.
+  override def create(cardboard: Cardboard): Task[List[Long]] = dbService.insert[CardboardRow](List(cardboard))
+
+  override def get: Task[List[Cardboard]] = dbService.get[Cardboard]
+//  override def get(cardboard: Cardboard): Task[Option[Cardboard]] = DBService.get[Cardboard]
+
+  override def update(cardboard: Cardboard): Task[Cardboard] = ???
+
+  override def delete(cardboard: Cardboard): Task[Cardboard] = ??? // todo return Cardboard or number of deleted cardboards + CHECK THE DELETE IN OBJECT
+
+  def findUser(userId: String): User = ???
 
 }
