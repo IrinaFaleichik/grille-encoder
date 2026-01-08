85ee00f2c1a4169cd1ba14b02dfdc1378cb49c3f
IrinaFaleichik <ira.faleichik@gmail.com>
2026-01-08 16:25:31 +0500

    Added a new spec for /users route

--- src/test/scala/infrastructure/http/UserRoutesSpec.scala
+++ src/test/scala/infrastructure/http/UserRoutesSpec.scala
@@ -3,6 +3,8 @@ package infrastructure.http
 
 import domain.model.User
 import infrastructure.db.repository.user.UserRepository
+
+import io.netty.handler.codec.http.multipart.HttpData
 import zio.Scope
 import zio.test.*
 import zio.http.*
@@ -11,6 +13,7 @@ import zio.test.Assertion.*
 import zio.ZIO
 import zio.ZLayer
 
+// todo add parsing json tests
 object UserRoutesSpec extends ZIOSpecDefault {
   val usersMock = List(User("1", "user1", List.empty))
   val userRepositoryMock: UserRepository = new UserRepository {
@@ -23,8 +26,8 @@ object UserRoutesSpec extends ZIOSpecDefault {
     override def delete(user: User): ZIO[Any, Throwable, User] = ???
   }
 
-  def spec: Spec[Any, Throwable] =
-    test("users test") {
+  def spec: Spec[Any, Throwable] = suite("http/users")(
+    test("returns users list as json") {
       for {
         client <- ZIO.service[Client]
         _ <- TestClient.addRoutes[UserRepository] {
@@ -33,5 +36,17 @@ object UserRoutesSpec extends ZIOSpecDefault {
         usersResponse <- client.batched(Request.get(URL.root / "users"))
         usersBody <- usersResponse.body.asString
       } yield assertTrue(usersBody == usersMock.toJson)
+    }.provide(TestClient.layer, ZLayer.succeed(userRepositoryMock)),
+
+    test("users route will ignore any json body") {
+      for {
+        client <- ZIO.service[Client]
+        _ <- TestClient.addRoutes[UserRepository] {
+          UserRoutes.getUsers
+        }
+        usersResponse <- client.batched(Request.get(URL.root / "users").withBody(Body.fromString("Some body")))
+        usersBody <- usersResponse.body.asString
+      } yield assertTrue(usersBody == usersMock.toJson)
     }.provide(TestClient.layer, ZLayer.succeed(userRepositoryMock))
+  )
 }
