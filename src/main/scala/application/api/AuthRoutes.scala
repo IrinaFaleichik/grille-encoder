05c588a969eabb460754d91b8f74af0ce2999cd5
IrinaFaleichik <ira.faleichik@gmail.com>
2026-02-02 22:27:39 +0500

    Fixed authentication at account/login route

--- src/main/scala/application/api/AuthRoutes.scala
+++ src/main/scala/application/api/AuthRoutes.scala
@@ -3,25 +3,20 @@ package application.api
 
 import application.api.*
 import application.api.auth.{AuthService, AuthUserDto}
-import infrastructure.logging.LoggingExtensions.logErrorWithoutTrace
-import application.api.auth.identity.Identity
-import domain.model.User
-
 import zio.ZIO
 import zio.http.*
-import zio.json.EncoderOps
 
 object AuthRoutes:
   val routes: Routes[AuthService, Response] = Routes(login, signUp)
 
-  lazy val login: Route[AuthService, Response] = (
+  lazy val login: Route[AuthService, Response] =
     Method.POST / "account" / "login" -> handler: (request: Request) =>
-      parseCredentials(request)
-        .flatMap(AuthService.authenticate)
-        .map: user =>
-          Response.text(s"Login succeed! Enjoy your stay, ${user.username}!")
-        .logErrorWithoutTrace(_.getMessage)
-    ).handleError(jsonParsingError + dbErrors + anyError)
+      for
+        _ <- ZIO.logInfo("entering route: /account/signup")
+        authUser <- ZIO.service[AuthUserDto]
+      yield Response.text(s"Welcome back, ${authUser.username}.")
+    .mapError(dbErrors + jsonParsingError + anyError) @@
+      Auth.basicAuthWithUserContext @@ redirectToGreet
 
   lazy val signUp: Route[AuthService, Response] =
     Method.POST / "account" / "signup" -> handler: (request: Request) =>
